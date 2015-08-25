df0bd1d04c01c622c44122a77263fcc4241234fc
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-08-25 17:46:34 +0200

    Added the option to compute the norm for any 2 principal components using the option "pcs".

--- R/measureLoadings.R
+++ R/measureLoadings.R
@@ -7,16 +7,23 @@
 #'
 #' @param pca \code{\link{prcomp}} object.
 #' @param varCutoff cumulative variance cutoff.
+#' @param pcs when set the function will compute the Euclidean norm for selected principal components
+#' instead of using the first \code{n} (overrides \code{varCutoff}). Vector of length 2.
 #'
 #' @return norms a vector with the Euclidean Norm of all the loadings.
 #'
 #' @export
 
-measureLoadings <- function(pca, varCutoff = 0.75) {
+measureLoadings <- function(pca, varCutoff = 0.75, pcs = NULL) {
     pc <- which(summary(pca)$importance[3,] > varCutoff)[1]
     norms <- c()
     for (i in 1:nrow(pca$rotation)){
-        norms <- c(norms, norm(as.matrix(pca$rotation[i, 1:pc])))
+
+        if (!is.null(pcs))
+            norms <- c(norms, norm(as.matrix(pca$rotation[i, pcs[1]:pcs[2]])))
+        else
+            norms <- c(norms, norm(as.matrix(pca$rotation[i, 1:pc])))
     }
+
     norms
 }
