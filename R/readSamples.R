756c6c2b91510ae79117404f2e6385aa0866a6ba
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-10-30 15:16:24 +0100

    Removed "method" argument. Probes are reduced by the max median expression rule. Speed optimization adjustments.

--- R/readSamples.R
+++ R/readSamples.R
@@ -1,31 +1,33 @@
 #' Import samples
 #'
+#' @param data data frame of matrix with gene expression values where rows
+#' represent genes and columns represent samples.
+#' @param genes vector of length \code{nrow(data)} containing gene names in
+#' "HGNC" format.
 #'
+#' @return kati gamato
 #'
-readSamples <- function(data, genes, method = c("max", "mean", "median")) {
+#' @export
+readSamples <- function(data, genes) {
 
-    match.arg(method, c("max", "mean", "median"))
-    print(method)
     nonNAgenes <- which(!is.na(genes))
     data <- data[nonNAgenes,]
 
     genes <- genes[nonNAgenes]
 
-    a<- mclapply(unique(genes), .testFun, data, genes, method)
+    a<- mclapply(unique(genes), .maxMedian, data, genes)
 
-    d <- do.call(rbind.data.frame, a)
+    d <- do.call(rbind, a)
+
     rownames(d) <- unique(genes)
     d
 }
 
+.maxMedian <- function(gene, data, genes) {
 
-.testFun <- function(gene, data, genes, method) {
-
-    if (method == "max") {
-        maxGene <- which.max(abs(apply(data[genes == gene, ], 1, mean)))
-        data[genes == gene,][maxGene, ]
-    } else
-        apply(data[genes == gene,], 2, method)
+    data <- data[genes == gene, ]
+    maxGene <- which.max(abs(apply(data, 1, median)))
+    data[maxGene, ]
 
 }
 
