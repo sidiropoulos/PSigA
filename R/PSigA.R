b5726cf824c3fb62877d4758a5262cda455b5b24
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-12-13 22:36:55 +0100

    Replaced TCGA AML with AK-AML from 4 cohorts. AML_meta deprecated as a separate call, now it's loaded with "data(AML)". Fixed big in MSigDB. Removed uneeded .rda files.

--- R/PSigA.R
+++ R/PSigA.R
@@ -10,34 +10,41 @@
 #' @name PSigA
 NULL
 
-#' Microarray data of 183 AML patients
+#' Microarray data of 424 AML patients
 #'
-#' Gene expression data of 183 AML patients, retrieved from TCGA, normalized
-#' and batch corrected. Rows represent microarray probes of the Affymetrix HGU
-#' 133 Plus2 platform. Each column represents an AML patient.
+#' Gene expression data of 424 AML patients with aberrant karyotyope (AK-AML),
+#' retrieved from TCGA, GSE15434, GSE13159 and GSE14468, normalized and batch
+#' corrected. Rows represent genes in HGNC format. Each column represents an
+#' AML patient.
+#' A data frame with clinical information on the karyotype, gender and age of
+#' the AK-AML patients is also loaded (\code{\link{AML_meta}}).
 #'
 #' @usage data(AML)
-#' @format A data frame with 54675 rows (Affy HGU 133 Plus 2 probes) and 183
+#' @format
+#' A data frame with 21369 rows (genes in HGNC format) and 424
 #' columns (patients).
 #'
 #' @source \url{https://tcga-data.nci.nih.gov/tcga/}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE15434}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE13159}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE14468}
 "AML"
 
 #' AML dataset metadata
 #'
-#' Clinical data of the TCGA AML patients.
+#' Clinical data for the \code{\link{AML}} dataset.
 #'
-#' @usage data(AML_meta)
-#' @format A data frame with 183 rows (patients) and 5 variables
+#' @usage data(AML)
+#' @format A data frame with 424 rows (patients) and 3 columns:
 #' \itemize{
 #'     \item karyotype: Cytogenetic aberration
 #'     \item gender: 1 = male, 0 = female
 #'     \item age: patient's age in years
-#'     \item OS: Survival from diagnosis until the last follow up in days
-#'     \item EventOS: Patient status by the time of the last follow up.
-#'           1 = dead, 0 = alive
 #' }
 #' @source \url{https://tcga-data.nci.nih.gov/tcga/}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE15434}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE13159}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE14468}
 "AML_meta"
 
 #' Molecular Signature DataBase
@@ -45,7 +52,7 @@ NULL
 #' 10,348 gene pathways obtained from Molecular Signature Database
 #' (MSigDB v5.0).
 #'
-#' @usage data(MSigDb)
+#' @usage data(MSigDB)
 #' @format An object of type list with 10,348 entries, each one representing a
 #' gene signature. The name of each list entry represents the signature's name.
 #' Additionally, \code{MSigDB} entries contain the following collection
