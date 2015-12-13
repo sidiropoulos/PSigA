3742a7f5250aff3e83bfbe6a2d4f430fc8073910
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-12-13 22:38:42 +0100

    Edit documentation, added example.

--- R/auxiliary_functions.R
+++ R/auxiliary_functions.R
@@ -4,16 +4,25 @@
 #' the given data matrix and gene-signature and returns the results as an object
 #' of class \code{prcomp}.
 #'
-#' @param signature character vector with the signature's gene identifiers
+#' @param signature character vector with the signature's gene identifiers.
 #' @param data Gene expression matrix where rownames correspond to unique gene
-#' identifiers (HGNC format \link{http://www.genenames.org}) and columns
-#' correspond to samples.
+#' identifiers in \code{signature} format and columns correspond to samples.
 #' @param center a logical value indicating whether the variables should be
 #' shifted to be zero centered. See \code{\link{prcomp}} for more details.
 #' @param scale a logical value indicating whether the variables should be
 #' scaled to have unit variance before the analysis takes place. See
 #' \code{prcomp} for more details.
 #' @param ... arguments passed to \code{prcomp}.
+#'
+#' @examples
+#'
+#' data(AML)
+#' data(MSigDB)
+#'
+#' sigPCA <- signaturePCA(MSigDB[["NIKOLSKY_BREAST_CANCER_7P15_AMPLICON"]], AML)
+#'
+#' # plot using sigPlot
+#' sigPlot(sigPCA, groups = AML_meta$karyotype)
 #'
 #' @export signaturePCA
 signaturePCA <- function(signature, data, center = TRUE, scale = FALSE, ...){
