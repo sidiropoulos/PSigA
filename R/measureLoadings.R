927bb1242d522fe508e230d4e94913d56fc2b39d
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-12-13 22:40:13 +0100

    "pcs" renamed to "pc" and now is a single value indicating the last pc to account for. Added example.

--- R/measureLoadings.R
+++ R/measureLoadings.R
@@ -5,24 +5,35 @@
 #'
 #' @param pca \code{\link{prcomp}} object.
 #' @param varCutoff cumulative variance cutoff.
-#' @param pcs when set the function will compute the Euclidean norm for
-#' selected principal components instead of using the first \code{n}
-#' (overrides \code{varCutoff}). Vector of length 2.
+#' @param pc when set the function will compute the Euclidean norm using the
+#' first \code{pc} principal components instead of using the first \code{n}
+#' (overrides \code{varCutoff}).
 #'
 #' @return norms a vector with the Euclidean Norm of all the loadings.
 #'
+#' @examples
+#'
+#' data(iris)
+#'
+#' pca <- prcomp(iris[, -5], scale = FALSE)
+#' measureLoadings(pca)
+#'
+#' #use only the first 2 principal components
+#' measureLoadings(pca, pc = 2)
+#'
 #' @export
 
-measureLoadings <- function(pca, varCutoff = 0.75, pcs = NULL) {
-    pc <- which(summary(pca)$importance[3,] > varCutoff)[1]
+measureLoadings <- function(pca, varCutoff = 0.75, pc = NULL) {
+
     norms <- c()
     for (i in 1:nrow(pca$rotation)){
 
-        if (!is.null(pcs))
-            norms <- c(norms, norm(as.matrix(pca$rotation[i, pcs[1]:pcs[2]])))
-        else
-            norms <- c(norms, norm(as.matrix(pca$rotation[i, 1:pc])))
+        if (is.null(pc))
+            pc <- which(summary(pca)$importance[3,] > varCutoff)[1]
+
+        norms <- c(norms, norm(as.matrix(pca$rotation[i, 1:pc])))
     }
 
+    names(norms) <- rownames(pca$rotation)
     norms
 }
